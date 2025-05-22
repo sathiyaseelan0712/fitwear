8eef04c34795488754d7a9e9fc73bdaea6b365c6
sathiyaseelan0712 <sathiyaseelan0712@gmail.com>
2025-05-22 07:22:44 +0530

    slight changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,11 +13,11 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className='text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA </p>
-          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
-          <p className='text-gray-600'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+          <p className='text-gray-500'>Salem Main Road <br /> Near Power Station, Komarapalayam, Tamil Nadu </p>
+          <p className='text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@fitwear.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at FitWear</p>
+          <p className='text-gray-600'>Learn more about our teams</p>
+          {/* <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button> */}
         </div>
       </div>
       <NewsletterBox/>
